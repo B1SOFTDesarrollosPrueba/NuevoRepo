8e61782d8e6345d7b48da636e7146616dc33ca17
Jorge Pierre <contacto@b1-soft.com>
2016-04-11 19:08:09 -0500

    Cambio en Prueba
    Se le agregaron dos lineas

--- Prueba.java
+++ Prueba.java
@@ -1,5 +1,7 @@
 public class Prueba{
 	public static void main(String args[]){
 		System.out.println("Prueba para B1Soft");
+		System.out.println("Otra linea");
+		System.out.println("No se me ocurre nada");
 	}
 }
